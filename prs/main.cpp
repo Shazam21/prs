3e59eda9a7982d7981ef9ad615974e8a53a7a321
Николай Костин <nikolajkostin@MacBook-Pro-Nikolaj.local>
2018-03-09 12:38:46 +0700

    Initial Commit

---
+++ prs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prs
+//
+//  Created by Николай Костин on 09.03.18.
+//  Copyright © 2018 Николай Костин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
